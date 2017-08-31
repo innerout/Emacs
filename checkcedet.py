ba2a50a161b546c389ced00ee9b84a2f54e69a37
Geroge Xanthakis <kompiouterakias@hotmail.com>
2017-08-31 23:55:56 +0300

    Fixed checkcedet if was always true and install.sh refactoring

--- checkcedet.py
+++ checkcedet.py
@@ -5,15 +5,15 @@ from pathlib import Path
 
 print("Cedet check is starting\n")
 
-if(not os.path.isdir("~/cedet")):
+if(not os.path.isdir(str(Path.home())+"/cedet")):
     home=str(Path.home())
     os.chdir(home)
     os.system('git clone http://git.code.sf.net/p/cedet/git cedet')
     home=str(Path.home())+"/cedet"
     os.chdir(home)
     os.system('make')
-    home2=str(Path.home())+"/cedet/contrib"
-    os.chdir(home2)
+    home=str(Path.home())+"/cedet/contrib"
+    os.chdir(home)
     os.system('make')
 
 print("Cedet is configured proceeding to uncompressing emacs tar\n")
--- install.sh
+++ install.sh
@@ -3,14 +3,12 @@ python checkcedet.py
 cd ~/
 if ( [  -d ~/.emacs.d ] && [  -f ~/.emacs ] ) then 
    echo "Backing up your configuration in oldemacs.tar.gz"
-   tar -cf oldemacs.tar.gz ~/.emacs.d ~/.emacs
-   mv oldemacs.tar.gz ~/
+   tar -cf oldemacs.tar.gz .emacs.d .emacs   
    elif([  -d ~/.emacs.d ]) then
        echo "Backing only your ~/.emacs.d folder in oldemacs.tar.gz  .emacs config does not exist"
-       tar -cf oldemacs.tar.gz ~/.emacs.d
-       mv oldemacs.tar.gz ~/
+       tar -cf oldemacs.tar.gz .emacs.d
    elif ([  -f ~/.emacs ]) then
 	echo "Backing only your ~/.emacs config in oldemacs.tar.gz .emacs.d folder does not exist"
-	tar -cf oldemacs.tar.gz ~/.emacs
+	tar -cf oldemacs.tar.gz .emacs
    fi
 	tar -xf emacs.tar.gz
