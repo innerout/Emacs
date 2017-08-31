25f11c7cf17eef3411ff28c098a7ec953a1f12fa
George Xanthakis <kompiouterakias@hotmail.com>
2017-08-31 12:30:12 +0300

    install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+mv emacs.tar.gz ~/
+python checkcedet.py
+cd ~/
+if ( [  -d ~/.emacs.d ] && [  -f ~/.emacs ] ) then 
+   echo "Backing up your configuration in oldemacs.tar.gz"
+   tar -cf oldemacs.tar.gz ~/.emacs.d ~/.emacs
+   mv oldemacs.tar.gz ~/
+   elif([  -d ~/.emacs.d ]) then
+       echo "Backing only your ~/.emacs.d folder in oldemacs.tar.gz  .emacs config does not exist"
+       tar -cf oldemacs.tar.gz ~/.emacs.d
+       mv oldemacs.tar.gz ~/
+   elif ([  -f ~/.emacs ]) then
+	echo "Backing only your ~/.emacs config in oldemacs.tar.gz .emacs.d folder does not exist"
+	tar -cf oldemacs.tar.gz ~/.emacs
+   fi
+	tar -xf emacs.tar.gz
