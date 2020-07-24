ed8e058d1203b22e5f90bd0aa5fc9a09b908a9fd
George Xanthakis <kompiouterakias@hotmail.com>
2020-07-24 20:50:52 +0300

    Add smarter move at the beginning of line

--- init.el
+++ init.el
@@ -104,6 +104,32 @@ FACE defaults to inheriting from default and highlight."
                  (or face '(:inherit default :inherit highlight)))
     ol))
 
+;;https://emacsredux.com/blog/2013/05/22/smarter-navigation-to-the-beginning-of-a-line/
+(defun smarter-move-beginning-of-line (arg)
+  "Move point back to indentation of beginning of line.
+
+Move point to the first non-whitespace character on this line.
+If point is already there, move to the beginning of the line.
+Effectively toggle between the first non-whitespace character and
+the beginning of the line.
+
+If ARG is not nil or 1, move forward ARG - 1 lines first.  If
+point reaches the beginning or end of the buffer, stop there."
+  (interactive "^p")
+  (setq arg (or arg 1))
+
+  ;; Move lines first
+  (when (/= arg 1)
+    (let ((line-move-visual nil))
+      (forward-line (1- arg))))
+
+  (let ((orig-point (point)))
+    (back-to-indentation)
+    (when (= orig-point (point))
+      (move-beginning-of-line 1))))
+
+(global-set-key (kbd "C-a") 'smarter-move-beginning-of-line)
+
 (setq x-stretch-cursor t)   ;; Make cursor the width of the character it is under
 (setq load-prefer-newer t)
 (setq sentence-end-double-space nil)
